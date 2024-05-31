29bd7cba434e80b02b1ed14e6ae435037689dd58
MiguelSilleza <98940269+Silleza-Miguel@users.noreply.github.com>
2024-05-31 19:49:31 +0800

    Add files via upload

---
+++ WpfApp4/Commands/SelectFloorCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using WpfApp4.Models;
+using WpfApp4.ViewModels;
+
+namespace WpfApp4.Commands
+{
+    internal class SelectFloorCommand : CommandBase
+    {
+        private FloorViewModel _floorViewModel;
+
+        public SelectFloorCommand(FloorViewModel floorViewModel)
+        {
+            _floorViewModel = floorViewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            Floor floor = (Floor)parameter;
+            Debug.WriteLine($"parameter is {floor.FloorID}");
+
+            _floorViewModel.SelectedFloor = floor;
+        }
+    }
+}
