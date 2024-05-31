db2979e9a557edf41b80b5cb7fac347dbf1893b7
MiguelSilleza <98940269+Silleza-Miguel@users.noreply.github.com>
2024-05-31 19:58:18 +0800

    Add files via upload

---
+++ WpfApp4/ViewModels/IdleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+using WpfApp4.Commands;
+using WpfApp4.Stores;
+
+namespace WpfApp4.ViewModels
+{
+    public class IdleViewModel : ViewModelBase
+    {
+        public ICommand BreakIdleNavigateCommand { get; set; }
+
+        public IdleViewModel(NavigationStore navigationStore, BuildingStore buildingStore)
+        {
+            BreakIdleNavigateCommand = new NavigateCommand<HomeViewModel>(buildingStore, new Services.NavigationService<HomeViewModel>(navigationStore, () => new HomeViewModel(navigationStore, buildingStore)));
+        }
+    }
+}
